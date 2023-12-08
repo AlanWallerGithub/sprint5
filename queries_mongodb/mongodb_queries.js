1c7aad5d61158e5813faa73324833bc9b91d560f
AlanWallerGithub <140154835+AlanWallerGithub@users.noreply.github.com>
2023-12-08 14:14:41 +0100

    cambios queries

--- queries_mongodb/mongodb_queries.js
+++ queries_mongodb/mongodb_queries.js
@@ -244,7 +244,17 @@ const result30 = await cursor30.toArray();
 const result31 = await cursor31.toArray();
 const result32 = await cursor32.toArray();
 
+let arrayResultados = [result1, result2,result3,result4,result5,result6,result7,result8,result9,result10,result11,result12,result13,result14,result15,result16,result17,result18,result19,result20,result21,result22,result23,result24,result25,result26,result27,result28,result29,result30,result31,result32];
+
+
 await client.close();
+
+for (let i = 0;i<arrayResultados.length;i++){
+  console.log("*************\nRESULTADO NUMERO "+(i+1)+":\n*************");
+  console.log(arrayResultados[i]);
+}
+
+
 
 }
 
